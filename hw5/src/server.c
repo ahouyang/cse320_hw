4bb9c25ae8dcec95e5a42cc2cfb695de5823bf53
Andy Ouyang <ahouyang@cs.stonybrook.edu>
2018-05-07 22:21:20 -0400

    fixed users

--- hw5/src/server.c
+++ hw5/src/server.c
@@ -105,7 +105,7 @@ void *bvd_client_service(void *arg){
         if(response == -1)
             break;
         parse_header(fd, hdr, payload);
-        free(*payload);
+        //free(*payload);
         free(payload);
         free(hdr);
     }
